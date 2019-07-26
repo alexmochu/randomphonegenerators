f77809d5f8609d8e30921660a850a393ff5a3d13
alexmochu <mochualex4@gmail.com>
2019-07-26 22:30:28 +0300

    setup CI/CD pipeline

--- src/AppComponent/App.test.js
+++ src/AppComponent/App.test.js
@@ -1,9 +1,40 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe('App Test', () => {
+  // let instance;
+  let shallowComponent;
+  beforeEach(() => {
+    shallowComponent = shallow(<App />);
+    // instance = shallowComponent.instance();
+  });
+
+  test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('should render once', () => {
+    expect(shallowComponent.length).toEqual(1)
+  });
+
+  // test('should set error to true and message', () => {
+  //   const event = {
+  //     preventDefault: () => {},
+  //   };
+  //   shallowComponent.setState({ limit: 20000 });
+  //   instance.generateRandomPhoneNumber(event);
+  //   const state = shallowComponent.state();
+  //   expect(state.limit).toEqual(20000);
+  //   expect(state.error).toBe(true);
+  //   expect(state.message).toBe("The number entered exceeds/below the accepted limit");
+  // });
+
 });
